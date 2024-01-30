907e49ac72084dc5693ea014d396d067c79e9e78
Stephen Margheim <stephen.margheim@gmail.com>
2024-01-30 15:02:05 +0100

    Initial commit
    $ bundle gem solid_metrics

---
+++ lib/solid_metrics.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "solid_metrics/version"
+
+module SolidMetrics
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_solid_metrics.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestSolidMetrics < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::SolidMetrics::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
